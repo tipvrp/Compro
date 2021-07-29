7746fe4bda2fdb6c80917c2f24436b2d855e45ce
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-07-29 23:03:00 +0700

    Add files via upload

---
+++ convert.c
@@ -0,0 +1,15 @@
+// ID 6101011621060
+// Name Veerapat Lamore
+// Section 1
+// Lab 1
+// Lecturer PIV
+#include <stdio.h>
+int main(void)
+{
+    int a,b ;
+    printf("Enter a lowercase letter:");
+    scanf("%c", &a);
+    b=a+32;
+    printf("You entered a lowercase of %c",b);
+    return 0;
+}
