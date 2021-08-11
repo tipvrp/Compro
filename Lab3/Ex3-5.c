ec84acbcbe1368453b3784b68d8b56d0050fa3a4
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-08-12 00:12:08 +0700

    Add files via upload

---
+++ Lab3/Ex3-5.c
@@ -0,0 +1,20 @@
+/*
+การวน loop ซ้อน loop
+*/
+
+#include <stdio.h>
+int main(void)
+{
+
+  int n=0;
+  for (int i= 0; i < 4; i++)
+  {
+        
+        for (int j = 0; j < 3; j++)
+        {
+            printf("count = %d\ni=%d j=%d\n",n++,i,j);
+        }
+    
+  }
+  return 0;
+}
