84232c4c430721ac2b953d77770e5a09225d6e10
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-08-11 07:06:18 +0700

    Create hw3-3.c

---
+++ Lab3/hw3-3.c
@@ -0,0 +1,25 @@
+//เนื้อร้องเพลง Five Little Ducks
+
+#include <stdio.h>
+int main(void)
+{
+  for(int i=7;i>0;i--)
+  {
+      if(i>2)
+        printf("\n\n%d little ducks went out one day\n",i-2);
+      else if (i==2)
+        printf("\n\nSad mother duck went out one day\n");
+      else 
+        printf("\n\n5 little ducks went out one day\n");
+      printf("Over the hill and far away\n");
+      printf("Mother duck said, \"Quack, quack, quack, quack\"\n");
+      if(i>3)
+      {
+        printf("But only %d little ducks came back\n",i-3);
+        for(int j=1;j<i-2;j++)  printf("%d ",j);    
+      }
+      else if(i==3) printf("But none of the five little ducks came back\n");
+      else if(i==2) printf("And all of five little ducks came back\n");
+      else printf("And all of the five little ducks came back!\n");
+  }
+}
