b5802909fbf39e19ce2c92aa9a9e1c29ce888fe9
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-08-13 16:32:28 +0700

    Update hw3-1.c

--- Lab3/hw3-1.c
+++ Lab3/hw3-1.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 int main(void)
 {
-  char a;
-  int b, c, count;
-  printf("please enter [o or e] [start] [stop]:");
-  scanf("%c %d %d", &a, &b, &c);
-  if (a == 'o')
-  {
-    if (b % 2 == 1)
-      count = b;
-    else
-      count = b + 1;
-  }
-  else if (a == 'e')
-  {
-    if (b % 2 == 0)
-      count = b;
-    else
-      count = b + 1;
-  }
-  else
-  {
-    printf("Error");
-    count = c + 1;
-  }
-  while (count <= c)
-  {
-    printf("%d ", count);
-    count += 2;
-  }
-  return 0;
+    char a;
+    int b, c;
+    printf("please enter [o or e] [start] [stop]:");
+    scanf("%c %d %d", &a, &b, &c);
+
+    while (b <= c)
+    {
+        if (a == 'o' && (b % 2) == 1)
+        {
+            printf("%d ", b);
+        }
+        if (a == 'e' && (b % 2) == 0)
+        {
+            printf("%d ", b);
+        }
+        b++;
+    }
+    return 0;
 }
