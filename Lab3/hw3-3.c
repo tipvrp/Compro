bd55d1ddde84bc1941a7f28e6972e8ae1fa6e40c
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-08-13 17:44:02 +0700

    Update hw3-3.c

--- Lab3/hw3-3.c
+++ Lab3/hw3-3.c
@@ -1,29 +1,29 @@
 //เนื้อร้องเพลง Five Little Ducks
-//138 words
+//105 words
 #include <stdio.h>
 int main(void)
 {
   for(int i=7;i>0;i--)
   {
       if(i>2)
-        printf("\n\n%d little ducks went out one day\n",i-2);
+        printf("\n\n%d little ducks ",i-2);
       else if (i==2)
-        printf("\n\nSad mother duck went out one day\n");
+        printf("\n\nSad mother duck ");
       else 
-        printf("\n\n5 little ducks went out one day\n");
-      printf("Over the hill and far away\n");
-      printf("Mother duck said, \"Quack, quack, quack, quack\"\n");
+        printf("\n\n5 little ducks ");
+      printf("went out one day\nOver the hill and far away\nMother duck said, \"Quack, quack, quack, quack\"\n");
       if(i>3)
       {
-        printf("But only %d little ducks came back\n",i-3);
+        printf("But only %d ",i-3);
+      }
+      else if(i==3) printf("But none of the five ");
+      else printf("And all of the five ");
+      printf("little ducks came back\n");
+      if(i>3)
         for(int j=1;j<i-2;j++)  
         {
             printf("%d",j);
             if(j<i-3)printf(", ");
         }
-      }
-      else if(i==3) printf("But none of the five little ducks came back\n");
-      else if(i==2) printf("And all of five little ducks came back\n");
-      else printf("And all of the five little ducks came back!\n");
   }
 }
