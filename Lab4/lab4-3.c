9cd2d30ae86003d0a73e708ba4fee2ec7407ba60
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-08-14 22:52:16 +0700

    Update lab4-3.c

--- Lab4/lab4-3.c
+++ Lab4/lab4-3.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-int main (void)
+int main(void)
 {
-// Local declarations
-int size, i;
-float sum=0, ave;
-//Statements
-printf ("How many numbers do you want to average? ");
-scanf ("%d",&size);
-//Create and fill variable-length array
-float ary[size];
-//Fill array and calculate sum
-for (i=0; i<size; i++){
-printf("Enter number %d :", i+1);
-scanf ("%f", &ary[ i ]);
-sum += ary[ i ];
-}
-ave = sum/size;
-//Print average
-printf("Average is: %f \nLower than average : ",ave);
+  // Local declarations
+  int size, i;
+  float sum = 0, ave;
+  //Statements
+  printf("How many numbers do you want to average? ");
+  scanf("%d", &size);
+  //Create and fill variable-length array
+  float ary[size];
+  //Fill array and calculate sum
+  for (i = 0; i < size; i++)
+  {
+    printf("Enter number %d :", i + 1);
+    scanf("%f", &ary[i]);
+    sum += ary[i];
+  }
+  ave = sum / size;
+  //Print average
+  printf("Average is: %f \nLower than average : ", ave);
 
-return 0;
+  return 0;
 }
