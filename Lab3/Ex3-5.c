ec3b6bd3f0b575788115dfae1e9f7c4b52c64a78
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-08-14 22:47:37 +0700

    Update Ex3-5.c

--- Lab3/Ex3-5.c
+++ Lab3/Ex3-5.c
@@ -5,16 +5,13 @@
 #include <stdio.h>
 int main(void)
 {
-
   int n=0;
   for (int i= 0; i < 4; i++)
   {
-        
         for (int j = 0; j < 3; j++)
         {
             printf("count = %d\ni=%d j=%d\n",n++,i,j);
-        }
-    
+        }    
   }
   return 0;
 }
