3e43f40cebf384d29c5ca3660f0948a2606d9295
tipvrp <69153130+tipvrp@users.noreply.github.com>
2021-08-14 22:51:21 +0700

    Update ex4.c

--- Lab4/ex4.c
+++ Lab4/ex4.c
@@ -1,58 +1,58 @@
 #include "stdio.h"
 #include "conio.h"
 #include "string.h"
-const n=5;
+const n = 5;
 int main()
 {
-    int i,j,nam[n],tmp;
-    char key;
-    for(i=0; i<n; i++)
+  int i, j, nam[n], tmp;
+  char key;
+  for (i = 0; i < n; i++)
+  {
+    printf("Enter Namber [%d]:", i + 1);
+    scanf("%d", &nam[i]);
+  }
+
+  do
+  {
+    printf("\n******Manu*******\n");
+    printf("\n1.Max to Min");
+    printf("\n2.Min to Max\n");
+    printf("Enter 1 or 2 :");
+    key = getche();
+
+  } while (!(key == '1' || key == '2'));
+
+  printf("\nData in Array Before Sort : ");
+  for (i = 0; i < n; i++)
+  {
+    printf("%5d", nam[i]);
+  }
+
+  for (j = 0; j < n - 1; j++)
+  {
+    if (key == '1')
     {
-      printf("Enter Namber [%d]:",i+1);    scanf("%d",&nam[i]);
-    }
-
-
- do{
- printf("\n******Manu*******\n");
- printf("\n1.Max to Min");
- printf("\n2.Min to Max\n");
- printf("Enter 1 or 2 :");
- key=getche();
-
- }while(!(key=='1' || key=='2'));
-
-
- printf("\nData in Array Before Sort : ");
- for(i=0;i<n;i++)
+      if (nam[j] < nam[j + 1])
       {
-             printf("%5d",nam[i]);
+        tmp = nam[j];
+        nam[j] = nam[j + 1];
+        nam[j + 1] = tmp;
       }
-
-       for(j=0;j<n-1;j++)
-             {
-                   if(key=='1')
-                        {
-                               if(nam[j]<nam[j+1])
-                                    {
-                                            tmp=nam[j];
-                                            nam[j]=nam[j+1];
-                                            nam[j+1]=tmp;
-                                     }
-                        }
-                else
-                        {
-                               if(nam[j]>nam[j+1])
-                                     {
-                                           tmp=nam[j];
-                                           nam[j]=nam[j+1];
-                                           nam[j+1]=tmp;
-                                      }
-                       }
-             }
-printf("\nData in Array After  Sort : ");
-for(j=0;j<n;j++)
-          {
-                printf("%5d",nam[j]);
-          }
-return 0;
+    }
+    else
+    {
+      if (nam[j] > nam[j + 1])
+      {
+        tmp = nam[j];
+        nam[j] = nam[j + 1];
+        nam[j + 1] = tmp;
+      }
+    }
+  }
+  printf("\nData in Array After  Sort : ");
+  for (j = 0; j < n; j++)
+  {
+    printf("%5d", nam[j]);
+  }
+  return 0;
 }
